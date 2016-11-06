7b19240f838848e49f38a188cb876d17c2a0386c
jn-gautier <jean.nicolas.gautier@gmail.com>
2016-11-06 21:55:43 +0100

    Create justifier1.py

---
+++ math/pythagore/justifier1.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
+
+"""On donne les dimensions d'un triangle, on précise qu'il est rectangle et on demande de choisir la bonne justification.
+Par exemple : le triangle ABC est rectangle en A et ses dimensions sont ... Parmi les propositions ci-dessous, laquelle permet de justifier qu'ABC est bien un triangle rectangle"""
