dda9b8640db65c959c2d5da4e6719a9edbb782c9
jn-gautier <jean.nicolas.gautier@gmail.com>
2016-11-06 21:51:43 +0100

    Create enoncer.py

---
+++ math/pythagore/enoncer.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
+
+"""On affiche un triangle rectangle avec le nom des sommets (SVG ou JSXgraph).
+On demande de complèter l'énoncé du théorème pour ce triangle.
+Les différentes parties de l'énoncé présentent un choix multiple.
+Par exemple : Le triangle ABC est rectangle en [choix] et son hypothénuse est [choix].
+Donc d'après le théorème de pythagore [choix]."""
