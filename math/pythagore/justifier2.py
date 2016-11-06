b9662805cfadda2b50deb1ba588935fd77a362df
jn-gautier <jean.nicolas.gautier@gmail.com>
2016-11-06 21:58:01 +0100

    Update justifier2.py

--- math/pythagore/justifier2.py
+++ math/pythagore/justifier2.py
@@ -1,1 +1,5 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
 
+"""On donne les dimensions d'un triangle, on précise qu'il n'est pas rectangle et on demande de choisir la bonne justification.
+Par exemple : le triangle ABC a les dimensions suivantes ... Parmi les propositions ci-dessous, laquelle permet de justifier qu'ABC n'est pas un triangle rectangle"""
